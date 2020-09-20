b9b4e5f1608bf040938fdb63f57c9bde07701484
glaxiphias <65498764+glaxiphias@users.noreply.github.com>
2020-09-20 21:24:35 +0300

    Add files via upload

---
+++ INPUT__girdi_ile_toplama.py
@@ -0,0 +1,15 @@
+ # input() Fonksiyonunu örneklendirmek için bir toplama işlemi:
+
+a=input("  ") #Burada tanımlanan a'nın türü string(Yani sözcük)dir.
+b=input("  ") #Burada tanımlanan b'nın türü string(Yani sözcük)dir.
+c=input("  ") #Burada tanımlanan c'nın türü string(Yani sözcük)dir.
+d=input("  ") #Burada tanımlanan d'nın türü string(Yani sözcük)dir.
+e=input("  ") #Burada tanımlanan e'nın türü string(Yani sözcük)dir.
+	      #Yukarıdaki sring'leri integer türüne çevirmemiz gerekiyor;çünkü kullanıcının girdiği değer bir sayı olacak.
+a=int(a)
+b=int(b)
+c=int(c)
+d=int(d)
+e=int(e)
+print("+______")
+print(" ",a+b+c+d+e)
